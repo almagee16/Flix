4a76fc5404e458369969e37e142186f130902d8f
Alvin Magee <alvinmagee@MacBook-Pro-30.dhcp.thefacebook.com>
2017-06-21 17:49:27 -0700

    Fully functional details page

--- Flix/NowPlayingViewController.swift
+++ Flix/NowPlayingViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import AlamofireImage
 
 class NowPlayingViewController: UIViewController, UITableViewDataSource {
-
+    
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -79,13 +79,22 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
     }
     
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let cell = sender as! UITableViewCell
+        if let indexPath = tableView.indexPath(for: cell) {
+        
+            let movie = movies[indexPath.row]
+            let detailViewController = segue.destination as! DetailViewController
+            detailViewController.movie = movie
+        }
+    }
     
     
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
 }

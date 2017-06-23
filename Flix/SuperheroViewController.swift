20d0525b7746e4e5ea055ee81d84622c07ecdba1
Alvin Magee <alvinmagee@MacBook-Pro-30.local>
2017-06-23 09:38:12 -0700

    Added ability to see a detailed view of the movie from the collections view with a tap.

--- Flix/SuperheroViewController.swift
+++ Flix/SuperheroViewController.swift
@@ -103,15 +103,16 @@ class SuperheroViewController: UIViewController, UICollectionViewDataSource {
                 // optional code for what happens after the alert controller has finished presenting
         }
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        let cell = sender as! UICollectionViewCell
+        if let indexPath = superheroCollectionView.indexPath(for: cell) {
+            
+            let movie = movies[indexPath.row]
+            let detailViewController = segue.destination as! DetailViewController
+            detailViewController.movie = movie
+        }
     }
-    */
+
 
 }
